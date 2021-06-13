c62019e1af5608dc5262db31885625b17af63d6a
deepak <kumar.de@husky.neu.edu>
2021-06-13 09:52:43 -0400

    Add location permission in manifest

--- app/src/main/java/edu/neu/madcourse/myapplication/LocatorActivity.java
+++ app/src/main/java/edu/neu/madcourse/myapplication/LocatorActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -38,8 +41,35 @@ public class LocatorActivity extends AppCompatActivity {
 
     }
 
-    private void getUserLocation(){
-        System.out.println("here");
+    private void getUserLocation() {
+        TextView textViewLocator = findViewById(R.id.textView_locator);
+        if (ActivityCompat.checkSelfPermission(LocatorActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            textViewLocator.setText(getResources().getString(R.string.Location_access_denied));
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, getResources().getInteger(R.integer.location_request_code));
+        } else if (ActivityCompat.checkSelfPermission(LocatorActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            textViewLocator.setText(getResources().getString(R.string.Location_access_denied));
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, getResources().getInteger(R.integer.location_request_code));
+        } else {
+            textViewLocator = findViewById(R.id.textView_locator);
+            Location currentGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location currentNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            Location currentPassiveLocation = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+            if (currentGPS != null) {
+                textViewLocator.setText(String.format(latitudeLongitudePair, currentGPS.getLatitude(), currentGPS.getLongitude()));
+            }
+            if (currentNetworkLocation != null) {
+                textViewLocator.setText(String.format(latitudeLongitudePair, currentNetworkLocation.getLatitude(), currentNetworkLocation.getLongitude()));
+            }
+            if (currentPassiveLocation != null) {
+                textViewLocator.setText(String.format(latitudeLongitudePair, currentPassiveLocation.getLatitude(), currentPassiveLocation.getLongitude()));
+            } else {
+                Snackbar.make(textViewLocator, gpsLoading, Snackbar.LENGTH_LONG).setAction("Action", null).show();
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, getResources().getInteger(R.integer.location_update_minimum_time), getResources().getInteger(R.integer.location_update_minimum_dist), v -> {
+                });
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, getResources().getInteger(R.integer.location_update_minimum_time), getResources().getInteger(R.integer.location_update_minimum_dist), v -> {
+                });
+            }
+        }
     }
 
 
